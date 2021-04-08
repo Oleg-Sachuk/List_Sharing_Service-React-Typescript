1fe4f1da6dde4de333bb19b1bd79c875a50398bc
Oleg-Sachuk <Sachuk.o.a@gmail.com>
2021-04-08 16:40:29 +0300

    Added email share form and logic

--- src/components/SharePage.tsx
+++ src/components/SharePage.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { Field, Form } from 'react-final-form';
 import { useHistory } from 'react-router-dom';
 import { IntForList } from './../interface';
 
@@ -34,6 +36,47 @@ const Share: React.FC = () => {
                         )
                     })}
                 </ul>
+            </div>
+            <div className="inpForm">
+                <div>
+                <Form
+            onSubmit={formData => {
+                const comparr: string[] = [''];
+                list.forEach(item => {
+                    if(item.completed) return comparr.push('%F0%9F%97%B9')
+                    else return comparr.push('%F0%9F%97%B7')
+                })
+                setTimeout(() => {
+                    window.location.href = ("mailto:" + formData.email + "?subject=" + formData.subject +
+                    "&body=" + list.map(item => `%0D%0A${item.title}%09 completed: ${comparr[list.indexOf(item)+1]}`));
+                }, 320);                
+            }}
+        >
+            {({ handleSubmit, pristine, submitting }) => (
+                <form onSubmit = {handleSubmit} >
+                    <Row>
+                        <Col>
+                            <p><b>Email:</b></p>
+                        </Col>
+                        <Col>
+                            <Field type={'email'} placeholder={'yourfreind@something.somthing'} name={'email'} component={'input'} />
+                        </Col>
+                        <Col>
+                            <p><b>Subject:</b></p>
+                        </Col>
+                        <Col>
+                            <Field type={'text'} placeholder={'Subject'} name={'subject'} component={'input'} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <button name={'btn'} disabled={submitting || pristine}>Send</button>
+                        </Col>
+                    </Row>
+                </form>
+            )}
+        </Form>
+                </div>
             </div>
             <div>
                 <button onClick={() => history.push('/')}>Back to your list</button>
